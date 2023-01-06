41a09b1f7b5f6879123e372554ec322bd7d0730e
Kristian Rahnev <mrchreesa@gmail.com>
2023-01-06 13:16:21 +0000

    fix: navbar responsivness

--- Components/NavBar.js
+++ Components/NavBar.js
@@ -1,5 +1,6 @@
 "use client";
-import { Disclosure } from "@headlessui/react";
+import { Fragment } from "react";
+
 import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
 import { FaFacebookSquare } from "react-icons/fa";
 import { GrInstagram } from "react-icons/gr";
@@ -7,6 +8,7 @@ import { ImLinkedin } from "react-icons/im";
 import { BsChatSquareDotsFill } from "react-icons/bs";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import { Popover, Transition } from "@headlessui/react";
 
 const navigation = [
   // { name: "Home", href: "/", current: true },
@@ -27,118 +29,128 @@ export default function Navbar() {
   } else {
     return (
       <>
-        <Disclosure as="nav" className="bg-transparent  ">
-          {({ open }) => (
-            <>
-              <div className="mx-auto  px-2 sm:px-6 lg:px-8 max-w-[1300px]">
-                <div className="relative flex h-20 items-center justify-between ">
-                  <div className="flex flex-1 items-center justify-center  sm:items-stretch sm:justify-start">
-                    <div className="flex  items-center ml-5">
-                      <Link href="/">
-                        <img
-                          className="md:ml-5"
-                          alt="logo"
-                          src="images/Nav-Logo.png"
-                          width="200"
-                        />
-                      </Link>
-                      {/* <img
-                    className="hidden h-10 w-auto lg:block"
-                    src="logo.png"
-                    alt="Kids Galaxy"
-                  /> */}
-                    </div>
-                    <div className="hidden lg:ml-10 lg:flex items-center">
-                      <div className="flex  space-x-4 items-center">
-                        {navigation.map((item) => (
-                          <Link
-                            key={item.name}
-                            href={item.href}
-                            className={classNames(
-                              "text-blue  hover:text-blue-500",
-                              "px-3 py-2 rounded-md text-base font-medium"
-                            )}
-                            aria-current={item.current ? "page" : undefined}
-                          >
-                            {item.name}
-                          </Link>
-                        ))}
-                      </div>
-                    </div>
-                    <div className="flex grow"></div>
-                    <div className="w-max hidden lg:flex items-center mr-5">
-                      <Link className="m-2" href={"/contact"}>
-                        <BsChatSquareDotsFill className="text-3v lg:text-2v md:max-w-[26px] md:mr-2" />
-                      </Link>
-                      <a
-                        className="m-2"
-                        href="https://www.instagram.com/kidsgalaxy.co.in/"
-                        target="_blank"
-                        rel="noreferrer"
-                      >
-                        <GrInstagram className="text-3v lg:text-2v md:max-w-[26px] md:mr-2" />
-                      </a>
-                      <a
-                        className="m-2"
-                        href="https://www.facebook.com/kidsgalaxy.co.in/"
-                        target="_blank"
-                        rel="noreferrer"
-                      >
-                        <FaFacebookSquare className="text-3v lg:text-2v md:max-w-[26px] md:mr-2" />
-                      </a>
-                      <a
-                        className="m-2"
-                        href="https://www.linkedin.com/company/kids-galaxy/about/"
-                        target="_blank"
-                        rel="noreferrer"
-                      >
-                        <ImLinkedin className="text-3v lg:text-2v md:max-w-[26px] md:mr-2" />
-                      </a>
-                    </div>
-                    <div className=" inset-y-0 right-0 flex items-center lg:hidden ">
-                      {/* Mobile menu button*/}
-                      <Disclosure.Button className="inline-flex items-center justify-center rounded-md p-2 text-gray-400  focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
-                        <span className="sr-only">Open main menu</span>
-                        {open ? (
-                          <XMarkIcon
-                            className="block h-6 w-6"
-                            aria-hidden="true"
-                          />
-                        ) : (
-                          <Bars3Icon
-                            className="block h-6 w-6"
-                            aria-hidden="true"
-                          />
+        <Popover className="relative bg-white z-10">
+          <div className="mx-auto  px-2 sm:px-6 lg:px-8 max-w-[1300px]">
+            <div className="relative flex h-20 items-center justify-between ">
+              <div className="flex flex-1 items-center justify-center  sm:items-stretch sm:justify-start">
+                <div className="flex  items-center ml-5">
+                  <Link href="/">
+                    <img
+                      className="md:ml-5"
+                      alt="logo"
+                      src="images/Nav-Logo.png"
+                      width="200"
+                    />
+                  </Link>
+                </div>
+                <div className="hidden lg:ml-10 lg:flex items-center">
+                  <div className="flex  space-x-4 items-center">
+                    {navigation.map((item) => (
+                      <Link
+                        key={item.name}
+                        href={item.href}
+                        className={classNames(
+                          "text-blue  hover:text-blue-500",
+                          "px-3 py-2 rounded-md text-base font-medium"
                         )}
-                      </Disclosure.Button>
+                        aria-current={item.current ? "page" : undefined}
+                      >
+                        {item.name}
+                      </Link>
+                    ))}
+                  </div>
+                </div>
+                <div className="flex grow"></div>
+                <div className="w-max hidden lg:flex items-center mr-5">
+                  <Link className="m-2" href={"/contact"}>
+                    <BsChatSquareDotsFill className="text-3v lg:text-2v md:max-w-[26px] md:mr-2" />
+                  </Link>
+                  <Link
+                    className="m-2"
+                    href="https://www.instagram.com/kidsgalaxy.co.in/"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    <GrInstagram className="text-3v lg:text-2v md:max-w-[26px] md:mr-2" />
+                  </Link>
+                  <Link
+                    className="m-2"
+                    href="https://www.facebook.com/kidsgalaxy.co.in/"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    <FaFacebookSquare className="text-3v lg:text-2v md:max-w-[26px] md:mr-2" />
+                  </Link>
+                  <Link
+                    className="m-2"
+                    href="https://www.linkedin.com/company/kids-galaxy/about/"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    <ImLinkedin className="text-3v lg:text-2v md:max-w-[26px] md:mr-2" />
+                  </Link>
+                </div>
+                <div className="-my-2 mt-2 lg:hidden">
+                  <Popover.Button className="inline-flex items-center justify-center rounded-md bg-white p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
+                    <span className="sr-only">Open menu</span>
+                    <Bars3Icon className="h-7 w-7" aria-hidden="true" />
+                  </Popover.Button>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <Transition
+            as={Fragment}
+            enter="duration-200 ease-out"
+            enterFrom="opacity-0 scale-95"
+            enterTo="opacity-100 scale-100"
+            leave="duration-100 ease-in"
+            leaveFrom="opacity-100 scale-100"
+            leaveTo="opacity-0 scale-95"
+          >
+            <Popover.Panel
+              focus
+              className="absolute z-10 inset-x-0 top-0 origin-top-right transform p-2 transition lg:hidden"
+            >
+              <div className="divide-y-2 z-10 divide-gray-50 rounded-lg bg-white shadow-lg ring-1 ring-black ring-opacity-5">
+                <div className="px-5 pt-5 pb-6">
+                  <div className="flex items-center justify-between">
+                    <Link href="/">
+                      <img
+                        className="md:ml-5"
+                        alt="logo"
+                        src="images/Nav-Logo.png"
+                        width="200"
+                      />
+                    </Link>
+                    <div className="-mr-2 ">
+                      <Popover.Button className="inline-flex items-center justify-center rounded-md bg-white p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
+                        <span className="sr-only">Close menu</span>
+                        <XMarkIcon className="h-7 w-7" aria-hidden="true" />
+                      </Popover.Button>
                     </div>
+                  </div>
+                  <div className="mt-6">
+                    <nav className="grid gap-y-8">
+                      {navigation.map((item) => (
+                        <Link
+                          key={item.name}
+                          href={item.href}
+                          className="-m-3 flex font-courier border-b items-center rounded-md p-3 hover:bg-gray-50"
+                        >
+                          <span className="ml-3 text-base font-medium text-gray-900">
+                            {item.name}
+                          </span>
+                        </Link>
+                      ))}
+                    </nav>
                   </div>
                 </div>
               </div>
-
-              <Disclosure.Panel className="sm:hidden">
-                <div className="space-y-1 px-2 pt-2 pb-3">
-                  {navigation.map((item) => (
-                    <Disclosure.Button
-                      key={item.name}
-                      as="a"
-                      href={item.href}
-                      className={classNames(
-                        item.current
-                          ? "bg-transparent text-blue"
-                          : "text-gray-300 hover:bg-gray-700 hover:text-white",
-                        "block px-3 py-2 rounded-md text-base font-medium"
-                      )}
-                      aria-current={item.current ? "page" : undefined}
-                    >
-                      {item.name}
-                    </Disclosure.Button>
-                  ))}
-                </div>
-              </Disclosure.Panel>
-            </>
-          )}
-        </Disclosure>
+            </Popover.Panel>
+          </Transition>
+        </Popover>
       </>
     );
   }
