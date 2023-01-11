eb5d179b61434f7fb2efc058431568185b1c134b
Kristian Rahnev <mrchreesa@gmail.com>
2023-01-11 17:14:59 +0000

    useSWR

--- Components/Contact/AppointmentsPage.js
+++ Components/Contact/AppointmentsPage.js
@@ -6,8 +6,11 @@ import useSWR from "swr";
 
 const AppointmentsPage = () => {
   // const [data, setData] = useState(null);
-  const { data, error, isLoading } = useSWR("/api/availabilityList", fetcher);
+  const fetcher = (url) => fetch(url).then((res) => res.json());
 
+  const url = "/api/availabilityList";
+  const { data, error, isLoading } = useSWR(url, fetcher);
+  console.log(data);
   // if (error) return <div>failed to load</div>;
   // if (isLoading) return <div>loading...</div>;
   // return <div>hello {data.name}!</div>;
