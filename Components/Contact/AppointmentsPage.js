f1c8d907806f622bd2c8b08c2e0831c09019cfa8
Kristian Rahnev <mrchreesa@gmail.com>
2023-01-11 18:25:17 +0000

    env

--- Components/Contact/AppointmentsPage.js
+++ Components/Contact/AppointmentsPage.js
@@ -5,21 +5,23 @@ import axios from "axios";
 import useSWR from "swr";
 
 const AppointmentsPage = () => {
-  const [data, setData] = useState(null);
-  // const fetcher = (url) => fetch(url).then((res) => res.json());
+  // const [data, setData] = useState(null);
+  const fetcher = (url) => fetch(url).then((res) => res.json());
 
-  // const url = "/api/availabilityList";
-  // const { data, error, isLoading } = useSWR(url, fetcher);
-  // console.log(data);
+  const url = "/api/availabilityList";
+  const { data, error, isLoading } = useSWR(url, fetcher);
+
   // if (error) return <div>failed to load</div>;
   // if (isLoading) return <div>loading...</div>;
   // return <div>hello {data.name}!</div>;
-  //${process.env.NEXT_PUBLIC_VERCEL_URL}/
-  useEffect(() => {
-    axios.get(`/api/availabilityList`).then((response) => {
-      setData(response.data);
-    });
-  }, []);
+
+  // useEffect(() => {
+  //   axios
+  //     .get(`${process.env.NEXT_PUBLIC_VERCEL_URL}/api/availabilityList`)
+  //     .then((response) => {
+  //       setData(response.data);
+  //     });
+  // }, []);
   // console.log(data);
   return (
     <div>
