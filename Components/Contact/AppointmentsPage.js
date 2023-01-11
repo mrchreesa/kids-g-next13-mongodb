66043b670fd59b881851d8887ab3febe34cf2a1c
Kristian Rahnev <mrchreesa@gmail.com>
2023-01-11 17:08:16 +0000

    useSWR

--- Components/Contact/AppointmentsPage.js
+++ Components/Contact/AppointmentsPage.js
@@ -2,17 +2,23 @@
 import React, { useState, useEffect } from "react";
 import AppointmentsCalender from "../Contact/AppointmentsCalender";
 import axios from "axios";
+import useSWR from "swr";
 
 const AppointmentsPage = () => {
-  const [data, setData] = useState(null);
+  // const [data, setData] = useState(null);
+  const { data, error, isLoading } = useSWR("/api/availabilityList", fetcher);
 
-  useEffect(() => {
-    axios
-      .get(`${process.env.NEXT_PUBLIC_VERCEL_URL}/api/availabilityList`)
-      .then((response) => {
-        setData(response.data);
-      });
-  }, []);
+  // if (error) return <div>failed to load</div>;
+  // if (isLoading) return <div>loading...</div>;
+  // return <div>hello {data.name}!</div>;
+
+  // useEffect(() => {
+  //   axios
+  //     .get(`${process.env.NEXT_PUBLIC_VERCEL_URL}/api/availabilityList`)
+  //     .then((response) => {
+  //       setData(response.data);
+  //     });
+  // }, []);
   // console.log(data);
   return (
     <div>
