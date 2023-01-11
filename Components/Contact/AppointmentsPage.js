ea8c9137fb5f4bfe1f107bc539a4baf6bffd22b8
Kristian Rahnev <mrchreesa@gmail.com>
2023-01-11 13:15:26 +0000

    new

--- Components/Contact/AppointmentsPage.js
+++ Components/Contact/AppointmentsPage.js
@@ -1,9 +1,19 @@
 "use client";
-import React from "react";
+import React, { useState, useEffect } from "react";
 import AppointmentsCalender from "../Contact/AppointmentsCalender";
+import axios from "axios";
 
-const AppointmentsPage = ({ data }) => {
-  console.log(typeof data);
+const AppointmentsPage = () => {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    axios
+      .get(`${process.env.NEXT_PUBLIC_API_URL}/api/availabilityList`)
+      .then((response) => {
+        setData(response.data);
+      });
+  }, []);
+  // console.log(data);
   return (
     <div>
       <AppointmentsCalender data={data} />
--- app/appointments/page.js
+++ app/appointments/page.js
@@ -1,31 +1,44 @@
 import React from "react";
 import AppointmentsPage from "../../Components/Contact/AppointmentsPage";
+import Users from "../../model/users";
+import connectDB from "../../lib/connectDB";
 // import { getData } from "../../pages/api/availabilityList";
 
-const getAvailableSlots = async () => {
-  const res = await fetch(
-    `${process.env.NEXT_PUBLIC_API_URL}/api/availabilityList`
-  );
-  if (!res.ok) {
-    console.log(res);
-    // throw new Error("Failed to fetch data ");
-  }
+// const getAvailableSlots = async () => {
+//   const res = await fetch(
+//     `${process.env.NEXT_PUBLIC_API_URL}/api/availabilityList`
+//   );
+//   if (!res.ok) {
+//     console.log(res);
+//     // throw new Error("Failed to fetch data ");
+//   }
 
-  return res.json();
-};
+//   return res.json();
+// };
 // const getAvailableSlots = async () => {
 //   const res = await getData();
 //   // console.log(res);
 //   return res;
 // };
+// const getAvailableSlots = async () => {
+//   await connectDB();
+//   console.log("Mongo");
+//   try {
+//     const availiblityList = await Users.find();
+//     // console.log(availiblityList);
+//     return availiblityList;
+//   } catch (err) {
+//     console.log(err);
+//   }
+// };
 
 const Appointments = async () => {
-  const data = await getAvailableSlots();
-  console.log(data);
+  // const data = await getAvailableSlots();
+  // console.log(data);
   return (
     <div className="App">
       <div className="section section0 fp-auto-height-responsive items-center">
-        <AppointmentsPage data={data} />
+        <AppointmentsPage /*data={data}*/ />
       </div>
     </div>
   );
